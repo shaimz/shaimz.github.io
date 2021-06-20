029039b223375cacb0796300faef5c2a35e208b9
shaimz <cvicol2@gmail.com>
2021-06-20 17:08:15 +0300

    te

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:shaimz/shaimz.github.io.git main:gh-pages
+git push -f git@github.com:shaimz/shaimz.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
