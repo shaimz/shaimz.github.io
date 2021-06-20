f7e8f764a3007106edc16082d173d1a7e6a4a92f
shaimz <cvicol2@gmail.com>
2021-06-20 16:14:10 +0300

    te

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('changes status on click then passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
