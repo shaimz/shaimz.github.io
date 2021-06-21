d7028fe6d69e97c5c4c69aa1d0a7fb72f03f079d
Shaimz <cvicol2@gmail.com>
2021-06-21 10:22:03 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import router from './router'
 import { store } from './store/store';
 
 const app = createApp(App);
-app.config.devtools =true;
 app.config.globalProperties.$store = store;
 app.use(store).use(router).mount('#app')
