257bf10432ebd70d58b638d085a267f3fd4ccbfb
Shaimz <cvicol2@gmail.com>
2021-07-10 14:18:37 +0300

    test

---
+++ resources/js/main.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello {{name}}</h1>
+</template>
+
+<script>
+    import {defineComponent} from '@vue/composition-api';
+    export default defineComponent({
+        setup(){
+            const name = ref('Cristi');
+
+            return{name}
+        }
+    })
+</script>
