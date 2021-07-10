d09f6f7236d7fe95937fdf95e4a5b44251befbba
Shaimz <cvicol2@gmail.com>
2021-07-10 12:57:42 +0300

    test

---
+++ src/components/upper.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home"></div>
+</template>
+
+<script lang="ts">
+    import {Vue} from "vue-class-component";
+
+    export default class upper extends Vue{
+
+    }
+</script>
